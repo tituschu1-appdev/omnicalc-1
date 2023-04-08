78f8a00c67bf05fbd445f543fba241b3132070c0
Titus Chu <tituschu@uchicago.edu>
2023-04-08 04:27:39 +0000

    did random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,22 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
     # params = {"number" => "42"}
-    @num = params.fetch("number").to_i
+    @num = params.fetch("number").to_f
     @square_of_num = @num ** 2 
     render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    # params = {"number" => "42"}
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/random_results.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 get("/square/new", { :controller => "application", :action => "blank_square_form"})
-
 get("/square/results", { :controller => "application", :action => "calculate_square"})
+
+get("/random/new", { :controller => "application", :action => "blank_random_form"})
+get("/random/results", { :controller => "application", :action => "calculate_random"})
 
 end
