fa5f9105d591271c50a1fb081af50f0f9936dc87
Titus Chu <tituschu@uchicago.edu>
2023-04-08 04:08:04 +0000

    square done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
+
+  def calculate_square
+    # params = {"number" => "42"}
+    @num = params.fetch("number").to_i
+    @square_of_num = @num ** 2 
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
